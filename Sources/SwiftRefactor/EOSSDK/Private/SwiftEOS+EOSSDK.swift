83d066407e8d27e80fc73c730e4a60e0b100afaf
Roman Dzieciol <roman.dzieciol@gmail.com>
2021-07-11 15:24:43 +0100

    feat: logging shims

--- Sources/SwiftRefactor/EOSSDK/Private/SwiftEOS+EOSSDK.swift
+++ Sources/SwiftRefactor/EOSSDK/Private/SwiftEOS+EOSSDK.swift
@@ -8,6 +8,39 @@ public var EOS_FALSE: EOS_Bool { 0 }
 
 public typealias EOS_NotificationId = UInt64
 
+
+/** A structure representing a log message */
+public struct EOS_LogMessage {
+    /** A string representation of the log message category, encoded in UTF-8. Only valid during the life of the callback, so copy the string if you need it later. */
+    let Category: UnsafePointer<CChar>
+    /** The log message, encoded in UTF-8. Only valid during the life of the callback, so copy the string if you need it later. */
+    let Message: UnsafePointer<CChar>
+    /** The log level associated with the message */
+    let Level: EOS_ELogLevel
+}
+
+
+public struct EOS_ELogLevel : Equatable, RawRepresentable {
+
+    public var rawValue: UInt32
+
+    public init(_ rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+
+    public init(rawValue: UInt32) {
+        self.rawValue = rawValue
+    }
+
+    public static var EOS_LOG_Fatal: Self { .init(100) }
+    public static var EOS_LOG_Error: Self { .init(200) }
+    public static var EOS_LOG_Warning: Self { .init(300) }
+    public static var EOS_LOG_Info: Self { .init(400) }
+    public static var EOS_LOG_Verbose: Self { .init(500) }
+    public static var EOS_LOG_VeryVerbose: Self { .init(600) }
+}
+
+
 public struct EOS_EResult : Equatable, RawRepresentable {
 
     public var rawValue: UInt32
---
+++ Sources/SwiftRefactor/EOSSDK/Public/Log/SwiftEOS+Log.swift
@@ -0,0 +1,32 @@
+
+import Foundation
+import os.log
+
+#if canImport(EOSSDK)
+import EOSSDK
+#endif
+
+extension OSLogType {
+
+    public static func from(eosLogLevel: EOS_ELogLevel) -> OSLogType {
+        switch eosLogLevel {
+        case .EOS_LOG_Fatal: return .fault
+        case .EOS_LOG_Error: return .error
+        case .EOS_LOG_Warning: return .error
+        case .EOS_LOG_Info: return .info
+        case .EOS_LOG_Verbose: return .info
+        case .EOS_LOG_VeryVerbose: return .info
+        default: return .default
+        }
+    }
+}
+
+extension Logger {
+
+    public static func log(subsystem: String = "dev.roman.eos", _ eosLogMessagePtr: UnsafePointer<EOS_LogMessage>?) {
+        guard let eosLogMessage = eosLogMessagePtr?.pointee else { return }
+        Logger(subsystem: subsystem, category: String(cString: eosLogMessage.Category))
+            .log(level: .from(eosLogLevel: eosLogMessage.Level), "\(String(cString: eosLogMessage.Message), privacy: .public)")
+    }
+
+}
